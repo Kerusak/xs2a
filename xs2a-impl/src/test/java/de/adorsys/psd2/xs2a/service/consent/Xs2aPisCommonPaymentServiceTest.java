18e1fbd29ec7877ae32297bb633e1e1ec4713482
Vitalii Yermak <vitalii.yermak@hotmail.com>
2019-02-22 03:29:01 +0200

    Compele tests for Xs2aPisCommonPaymentService

--- xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/consent/Xs2aPisCommonPaymentServiceTest.java
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/consent/Xs2aPisCommonPaymentServiceTest.java
@@ -16,10 +16,8 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-
 import java.util.Collections;
 import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
